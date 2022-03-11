87376602e1f6a017a39ce9129b9f667249859839
KW <kenneth_sl_wong@msn.com>
2022-03-11 18:08:37 -0500

    added employee adding

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(7,2) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
